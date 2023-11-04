20d51fc3f5b9a9de0de8ce0b208654a369e3eb56
RenTensei <scs.bon7@gmail.com>
2023-11-04 22:05:11 +0100

    Moved Git repository from PlatformService to project

---
+++ CommandsService/Startup.cs
@@ -0,0 +1,47 @@
+using Microsoft.OpenApi.Models;
+
+namespace CommandsService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthorization();
+            services.AddControllers();
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandsService", Version = "v1" });
+            // });
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                // app.UseSwagger();
+                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CommandsService v1"));
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.DTOs;
+using PlatformService.Models;
+using PlatformService.SyncDataServices.Http;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+        private readonly ICommandDataClient _commandDataClient;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper, ICommandDataClient commandDataClient)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _commandDataClient = commandDataClient;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDTO>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms...");
+
+            var platformItem = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDTO>>(platformItem));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDTO> GetPlatformById(int id)
+        {
+            Console.WriteLine("--> Getting Platforms...");
+
+            var platformItem = _repository.GetPlatformById(id);
+
+            if ( platformItem != null ) { return Ok(_mapper.Map<PlatformReadDTO>(platformItem)); }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PlatformReadDTO>> CreatePlatform(PlatformCreateDTO platformCreateDTO)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreateDTO);
+            _repository.CreatePlatform(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDTO = _mapper.Map<PlatformReadDTO>(platformModel);
+
+            try
+            {
+                await _commandDataClient.SendPlatformToCommand(platformReadDTO);
+            }
+            catch ( System.Exception ex )
+            {
+                Console.WriteLine($"--> Count not send synchronously {ex.Message}");
+            }
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDTO.Id }, platformReadDTO);
+        }
+    }
+}
